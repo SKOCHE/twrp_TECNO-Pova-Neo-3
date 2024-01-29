3e93d13cde1e04ab531386f9181daa8e6222bed0
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2024-01-29 09:41:44 +0700

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_LH6n.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_LH6n-eng \
+    twrp_LH6n-userdebug \
+    twrp_LH6n-user
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Tecno Pova Neo 3
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+
+```
